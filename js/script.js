9e8cea2938bc814643bbb54cf9c6c8ffe2ed6107
Andrey Belyakov <50678321+andrey-belyakov@users.noreply.github.com>
2022-06-23 23:32:11 +0300

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+new fullpage('#fullPage', {
+    autoscrolling: true,
+    navigation: true,
+    credits: false,
+    anchors: ['section1', 'section2', 'section3'],
+    navigationTooltips: ['Home', 'About', 'Contact'],
+    showActiveTooltip: true,
+    scrollingSpeed: 1000,
+    controlArrows: false,
+    slidesNavigation: true
+
+})
